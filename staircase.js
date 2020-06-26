ad89cb83eeeba1c00afab7fd72b063e760f21c70
Alef3 <47638899+Alef3@users.noreply.github.com>
2020-06-25 22:55:44 -0300

    Refatora
    .

--- staircase.js
+++ staircase.js
@@ -2,22 +2,23 @@
 'use strict'
 <!--12345# -->
 <!--1234## -->
-<!--123### -->
+<!--123### -->    <!--saída -->
 <!--12#### -->
 <!--1##### -->
 <!--###### -->
-let n = 6; let j = 0;
-for(let i = 1; i <= n; i++){
-	for(j = i; j < n; j++){
-    	document.write('-')
-    }
-    for(let k = 0; k < i; k++){
-    	document.write('#')
+
+//tamanho da escada(n por n)
+let n = 6;
+
+for(var i = 1; i <= n; i++){
+	for(var j = i; j < n; j++){
+    		document.write('-')
+    	}
+    	for(var k = 0; k < i; k++){
+    		document.write('#')
 	}
     	if(j == n){
     		document.write('<br>')
     	}
 }
-
-
 </script>
