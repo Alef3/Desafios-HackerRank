f9f3832b42f8dc4ff6efbabbad407929b1e426d9
Alef3 <47638899+Alef3@users.noreply.github.com>
2020-09-29 01:13:32 -0300

    refatora

--- Apples and Oranges.js
+++ Apples and Oranges.js
@@ -1,26 +1,18 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-
-    var numDeMacas = 0;
-    var numDeLaranjas = 0;
-
-    var macas_casa = ''
-    var laranjas_casa = ''
+    var quantidadeDeMacas = 0;
+    var quantidadeDeLaranjas = 0;
     
-    //Lento. Otimizar depois.
-    for (let i = 0; i < apples.length || i < oranges.length; i++) {
-        macas_casa = apples[i] + a
-        laranjas_casa = oranges[i] + b
-        for (let j = s; j <= t; j++) {
-            if (j == macas_casa) {
-                numDeMacas++;
-            } if (j == laranjas_casa) {
-                numDeLaranjas++;
-            }
+    //Corrigido!
+    for (let i = 0; i < b; i++) {
 
-        }
+        //verifica se as maçãs e laranjas cairão na casa do Sam
+        if (apples[i] + a >= s && apples[i] + a <= t) quantidadeDeMacas++;
+        
+        if (oranges[i] + b >= s && oranges[i] + b <= t) quantidadeDeLaranjas++;
 
     }
     
-    console.log(numDeMacas);
-    console.log(numDeLaranjas);
+    console.log(quantidadeDeMacas);
+    console.log(quantidadeDeLaranjas);
+
 }
